4491d0bf56e121ce47b27f8e6b953ad75cbe4aa7
OndraV <ondrej.velechovsky@student.spsmb.cz>
2023-10-20 09:30:09 +0200

    clicker

--- res/js/main.js
+++ res/js/main.js
@@ -1,16 +1,52 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade");
+const autoClickerUpgrade = document.getElementById("autoClickerUpgrade");
 
 let numberOfCookies = 0;
+let cookieIncreaseNumber = 1;
+let autoclickerCookieIncreaseNumber = 0;
+let autoclickerInterval;
+let clickUpgradePrice = 50;
 
-//.onclick -  na kliknutí 
-//()=> arrow funkce 
+//.onclick -  na kliknutí
+//()=> arrow funkce
 //{} - scope
 cookie.onclick = () => {
-    //zvednout číslo o 1 
-    //numberOfCookies = numberOfCookies +1;
-    numberOfCookies +=1;
-    //numberOfCookiess ++;
-    //zobrazit v odstavci 
-    counter.innerText = "Orea:"+ numberOfCookies;
-}
+  //zvednout číslo o 1
+  //numberOfCookies = numberOfCookies +1;
+  numberOfCookies += cookieIncreaseNumber;
+  //numberOfCookiess ++;
+  //zobrazit v odstavci
+  counter.innerHTML = "Orea:" + numberOfCookies;
+};
+clickUpgrade.onclick = () => {
+if (numberOfCookies >= clickUpgradePrice) {
+    //odečíst cenu nakupu
+    //numberOfCookies = numberOfCookies - 50
+    numberOfCookies -= clickUpgradePrice;
+    clickUpgradePrice *=2;
+    clickUpgrade.innerHTML = "Buy click upgrade: $
+    counter.innerHTML = "Orea:" + numberOfCookies;
+    //zvednout klikání o 1
+    cookieIncreaseNumber++;
+  }
+};
+
+autoClickerUpgrade.onclick = () => {
+  if (numberOfCookies >= 100) {
+    //odečteme cenu
+    numberOfCookies -= 100;
+    //zobrazíme počet sušeneek
+    counter.innerHTML = "Orea:" + numberOfCookies;
+    autoclickerCookieIncreaseNumber++;
+    //zastavíme bezici interval
+    clearInterval(autoclickerInterval);
+    //spustíme autoclicker
+    autoclickerInterval = setInterval(() => {
+      numberOfCookies++;
+      counter.innerHTML = "Orea:" + numberOfCookies;
+      numberOfCookies += autoclickerCookieIncreaseNumber;
+    }, 1000);
+  }
+};
